49dc3a8adab7266ec712f2a210e620140d149cbb
dqt12hcmus <37431768+dqt12hcmus@users.noreply.github.com>
2019-01-09 13:07:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const APP_PORT = 5555
+const APP_PORT = process.env.PORT || 5555
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
